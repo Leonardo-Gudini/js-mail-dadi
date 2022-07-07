ca9fc13fbe1e48e99d719e8a86f14341a40139fc
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-07-07 15:31:09 +0200

    aggiunto codice js controllo mail(da sistemare)

--- README.md
+++ README.md
@@ -10,3 +10,14 @@ GIOCO DEI DADI
     -crea condizione, se variabile computer > utente allora "computer ha vinto"
     -crea condizione, se variabile computer < utente allora "utente ha vinto"
 
+
+CHECK MAIL LIST
+
+-inizializza un array con l'elenco delle mail
+-chiedi all'utente la propria mail
+-confronta la mail inserita dall'utente con le mail contenute nell'array
+    -se mail è presente
+        "congratulazioni sei in lista"
+    -se mail non è presente
+        "purtroppo non sei in lista"
+
---
+++ mail/js/mail.js
@@ -0,0 +1,45 @@
+const mailList = ["mario.rossi@mail.it","lucia30@mail.com","steve.jobs@apple.it"]
+
+// for ( let i = 0; i < mailList.length; i++) {
+//     console.log(i, mailList[i]);
+// }
+const userMail = document.getElementById("mail");
+
+document.getElementById("btn").addEventListener("click",
+
+    function(){
+        for (let i = 0; i < mailList.length; i++){
+
+            if(userMail === mailList[1]){
+                console.log("puoi accedere");  
+            }
+            else{
+                console.log("non puoi accedere")
+            }
+             
+        }
+    }
+)
+
+
+for (let i = 0; i < mailList.length; i++){
+
+    if(userMail == mailList[1]){
+        console.log("puoi accedere");  
+    }
+     
+}
+
+
+
+
+// document.getElementById("btn").addEventListener("click",
+
+    // function(){
+        // const userMail = document.getElementById("mail").value;
+
+        
+        // console.log(userMail);
+    // }
+// )
+
