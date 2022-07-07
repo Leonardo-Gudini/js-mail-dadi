2fdf99972033acad06ca54c7840f9b49c71aa5d4
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-07-07 22:12:20 +0200

    fixed check mail

--- mail/js/mail.js
+++ mail/js/mail.js
@@ -1,45 +1,25 @@
 const mailList = ["mario.rossi@mail.it","lucia30@mail.com","steve.jobs@apple.it"]
 
-// for ( let i = 0; i < mailList.length; i++) {
-//     console.log(i, mailList[i]);
-// }
 const userMail = document.getElementById("mail");
 
 document.getElementById("btn").addEventListener("click",
 
     function(){
-        for (let i = 0; i < mailList.length; i++){
 
-            if(userMail === mailList[1]){
-                console.log("puoi accedere");  
+        // console.log(userMail.value);
+        // console.log(mailList[0]);
+
+        for (let i = 0; i < mailList.length; ++i){
+
+            console.log(mailList[i])
+
+            if(userMail.value == mailList[i]){
+                alert("Congratulazioni sei in lista!")
             }
-            else{
-                console.log("non puoi accedere")
-            }
-             
+            else {
+            } 
         }
     }
+
+    
 )
-
-
-// for (let i = 0; i < mailList.length; i++){
-
-//     if(userMail == mailList[1]){
-//         console.log("puoi accedere");  
-//     }
-     
-// }
-
-
-
-
-// document.getElementById("btn").addEventListener("click",
-
-    // function(){
-        // const userMail = document.getElementById("mail").value;
-
-        
-        // console.log(userMail);
-    // }
-// )
-
